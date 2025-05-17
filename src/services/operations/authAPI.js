669a7d2c6b2ec89f2c4648fd561113ec9ec80f13
LakhanSharma0 <is670772@gmail.com>
2025-05-17 22:03:31 +0530

    commented some console.log's

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -23,7 +23,7 @@ export function sendOtp(email, navigate) {
         email,
         checkUserPresent: true,
       })
-      console.log("SENDOTP API RESPONSE............", response)
+//      console.log("SENDOTP API RESPONSE............", response)
 
       console.log(response.data.success)
 
@@ -66,7 +66,7 @@ export function signUp(
         otp,
       })
 
-      console.log("SIGNUP API RESPONSE............", response)
+//      console.log("SIGNUP API RESPONSE............", response)
 
       if (!response.data.success) {
         throw new Error(response.data.message)
